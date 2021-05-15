8c3ff28a9ad1f84d79f988b5634b4d40bc138dee
Rudra <trivedirudra2@gmail.com>
2021-05-15 04:27:47 -0400

    Added Message reply

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const client = new Discord.Client();
 client.on("ready", () => {
     console.log(client.user.tag + " Online !");
 
-})
+});
+
+client.on("message", msg =>{
+    if(msg.author.tag === "chikoo#3169")
+    {
+        msg.reply("mah man !");
+    }
+});
+
+
+
+client.login("ODQzMDMyMjQ5ODc1ODI0NjUw.YJ989w.2FGsObRh6aE1h3Z8nEUqXE82P_s");
